5d68a0694b69d9cf24508d07f3899340c44b1df6
Akash27 <116902197+Akashwebb@users.noreply.github.com>
2025-01-29 17:39:33 +0530

    Update fun.js
    m

--- ARRAY Funcrions/fun.js
+++ ARRAY Funcrions/fun.js
@@ -37,6 +37,13 @@
 //    return x!==5 && x!==7  
 // }
 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+
+console.log(evenNumbers); // Output: [2, 4, 6, 8, 10]
+
+
 // let arr=[1,2,3,4]
 // let x= arr.filter((val)=>{
 //     return val!==3
